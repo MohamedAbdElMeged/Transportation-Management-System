6948c48bf46d54fe10df8dcfd05a0f41b38a1c81
Mohamed Abd El-Meged <mabdelmeged.se@gmail.com>
2023-06-17 04:25:32 +0300

    fix rubocop offenses

--- app/interactors/drivers/authenticate.rb
+++ app/interactors/drivers/authenticate.rb
@@ -1,21 +1,19 @@
 module Drivers
-    class Authenticate
-        include Interactor
-        def call
-            unless context&.[]('email') && context&.[]('password')
-                context.fail!(message: "email and password should be sent")
-            end
-            context.email = context&.[]('email')
-            context.password = context&.[]('password')
-            authenticate
-        end
-        private
-        def authenticate
-            driver = Driver.find_by(email: context.email)
-            unless driver && driver&.authenticate(context.password)
-                context.fail!(message: "invalid email/password")
-            end    
-            context.driver = driver 
-        end
+  class Authenticate
+    include Interactor
+    def call
+      context.fail!(message: 'email and password should be sent') unless context&.[]('email') && context&.[]('password')
+      context.email = context&.[]('email')
+      context.password = context&.[]('password')
+      authenticate
     end
+
+    private
+
+    def authenticate
+      driver = Driver.find_by(email: context.email)
+      context.fail!(message: 'invalid email/password') unless driver && driver&.authenticate(context.password)
+      context.driver = driver
+    end
+  end
 end
