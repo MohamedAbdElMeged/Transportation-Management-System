8b2aa07433a738624df2ad3e1535effebf3b6131
Mohamed Abd El-Meged <mabdelmeged.se@gmail.com>
2023-06-19 02:08:24 +0300

    delete next_page from redis after worker finish

--- app/workers/fetch_trucks_worker.rb
+++ app/workers/fetch_trucks_worker.rb
@@ -3,14 +3,19 @@ class FetchTrucksWorker
   sidekiq_options queue: :fetch_trucks_queue
   def perform
     response = Trucks::FetchNewTrucks.call
-    return if response.failure?
+    if response.failure?
+      REDIS.del('next_page')
+      return
+    end
 
     total_trucks_from_headers = response.response_headers&.[]('total-count')
-    total_trucks_from_db = REDIS.get('totalf_trucks_count_in_db')
+    total_trucks_from_db = REDIS.get('total_trucks_count_in_db')
     while total_trucks_from_db.to_i < total_trucks_from_headers.to_i
       response = Trucks::FetchNewTrucks.call
       break if response.failure?
+
       total_trucks_from_db = REDIS.get('total_trucks_count_in_db')
     end
+    REDIS.del('next_page')
   end
 end
